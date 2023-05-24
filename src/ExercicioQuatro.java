027b6fe8126b64b07c4901e6aabacbfcc8d994e1
PedrorOliva <pedro.robson@zup.com.br>
2023-05-24 10:36:20 -0300

    feat: adicionando exercicio 4

---
+++ src/ExercicioQuatro.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/*
+* Faça um Programa que recebe o quanto você ganha por hora e o número de horas trabalhadas no mês.
+* Calcule e mostre o total do seu salário no referido mês.
+* */
+public class ExercicioQuatro {
+  public static void main(String[] args) {
+    Scanner inputUsuario = new Scanner(System.in);
+
+    System.out.println("informe o valor da hora trabalhada: ");
+    float valorHora = inputUsuario.nextFloat();
+    System.out.println("informe a quantidade de horas que você trabalhou: ");
+    float horaTrabalhada = inputUsuario.nextFloat();
+
+    float salarioMes = valorHora * horaTrabalhada;
+
+    System.out.println("Total a receber no referido mês: RS " + salarioMes);
+  }
+}
