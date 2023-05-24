4964de7de3eafc056724c85caa2c9e81bfd2d093
PedrorOliva <pedro.robson@zup.com.br>
2023-05-24 10:23:54 -0300

    feat: adicionando exercicio 3

---
+++ src/ExercicioTres.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+// Faça um programa que converta metros para centímetros.
+public class ExercicioTres {
+  public static void main(String[] args) {
+    Scanner inputUser = new Scanner(System.in);
+
+    System.out.println("Informe a quantidade de metros que deseja converter: ");
+    float metros  = inputUser.nextFloat();
+
+    float centimetros = metros * 10;
+
+    System.out.println("A conversão de " + metros + " para centímetros é: " + centimetros);
+  }
+}
