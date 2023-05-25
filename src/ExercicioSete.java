15d38b4c3d5701b77c1671aa4dd8babe33497846
PedrorOliva <pedro.robson@zup.com.br>
2023-05-25 11:30:29 -0300

    fix: corrigindo formatação da saída

--- src/ExercicioSete.java
+++ src/ExercicioSete.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 /*
-* Faça um Programa que receba o quanto você ganha por hora e o número de
-* horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido
-* mês, sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o
-* INSS e 5% para o sindicato, faça um programa que nos dê respectivamente:
-* a) Salário bruto.
-* b) Quanto pagou ao INSS.
-* c) Quanto pagou ao sindicato.
-* d) Qual o salário líquido do funcionário.
+ * Faça um Programa que receba o quanto você ganha por hora e o número de
+ * horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido
+ * mês, sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o
+ * INSS e 5% para o sindicato, faça um programa que nos dê respectivamente:
+ * a) Salário bruto.
+ * b) Quanto pagou ao INSS.
+ * c) Quanto pagou ao sindicato.
+ * d) Qual o salário líquido do funcionário.
  * */
 
 public class ExercicioSete {
@@ -26,18 +26,18 @@ public class ExercicioSete {
 
     double salarioBruto = horaTrabalhada * valorHora;
 
-    double descontoImposto = salarioBruto * (percImposto/100);
+    double descontoImposto = salarioBruto * (percImposto / 100);
     double inss = salarioBruto * (percInss / 100);
     double sindicato = salarioBruto * (percSindicato / 100);
 
     double descontos = descontoImposto + inss + sindicato;
     double salarioLiquido = salarioBruto - descontos;
 
-    System.out.println("Salario bruto: R$" + salarioBruto);
+    System.out.printf("Salario bruto: R$%,.2f %n", salarioBruto);
     System.out.println("Pagou ao imposto de renda: R$" + descontoImposto);
     System.out.println("Pagou ao INSS: R$" + inss);
     System.out.println("Pagou ao sindicato: R$" + sindicato);
-    System.out.println("Salário líquido do funcionário: R$" + salarioLiquido);
+    System.out.printf("Salário líquido do funcionário: R$%,.2f" , salarioLiquido);
 
   }
 }
